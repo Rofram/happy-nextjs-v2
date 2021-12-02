68e5ef5885ef397c614d9316328eaa7c89bba187
Rodrigo de França <howdrigo.fr@gmail.com>
2021-12-02 00:19:24 -0300

    feat: add mapProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,11 @@ import '../styles/globals.css';
 import 'leaflet/dist/leaflet.css';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { MapProvider } from '../contexts/mapContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <MapProvider>
       <Head>
         <title>Happy :)</title>
         <meta charSet="utf-8" />
@@ -13,7 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" href="favicon.ico" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </MapProvider>
   );
 }
 
