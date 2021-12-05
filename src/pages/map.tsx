6f818b93855991875b75f6b63595a8821a57fc21
Rodrigo de França <howdrigo.fr@gmail.com>
2021-12-05 14:56:12 -0300

    feat: implemented map page

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,5 +1,28 @@
-import OrphanagesMap from "../templates/OrphanagesMap";
+import { GetStaticProps } from "next";
+import api from "../services/api";
+import { OrphanageDetailsProps } from "../templates/OrphanageDetails";
+import OrphanagesMap, { OrphanagesMapProps } from "../templates/OrphanagesMap";
 
-export default function Map() {
-  return <OrphanagesMap />
+export default function Map(props: OrphanagesMapProps) {
+  return <OrphanagesMap {...props} />
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get<OrphanageDetailsProps[]>('/orphanages')
+
+  const orphanages = data.map(orphanage => ({
+    position: [orphanage.latitude, orphanage.longitude],
+    popup: {
+      title: orphanage.name,
+      image: orphanage.images[0].url,
+      buttonLink: `/orphanages/${orphanage.id}`,
+    }
+  }))
+
+  return {
+    props: {
+      revalidate: 60 * 60,
+      orphanages
+    },
+  };
 }
