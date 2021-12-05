1fa4e05e725ddfaf7bf8e6b9f226baaf4b46d1bb
Rodrigo de França <howdrigo.fr@gmail.com>
2021-12-05 01:57:35 -0300

    feat: implemented orphanagesCreate template

--- src/templates/OrphanagesCreate/index.tsx
+++ src/templates/OrphanagesCreate/index.tsx
@@ -1,38 +1,67 @@
+import { useState, useRef, useCallback } from 'react';
 import dynamic from 'next/dynamic';
+
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 
 import PrimaryButton from "../../components/PrimaryButton";
 import Sidebar from "../../components/Sidebar";
+import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+
+import { FiPlus, FiUser } from "react-icons/fi";
+import mapIcon from '../../utils/map/mapIcon';
+import { useMapContext } from '../../contexts/mapContext';
 
 import style from './styles.module.scss';
-import { FiPlus } from "react-icons/fi";
-import mapIcon from '../../utils/map/mapIcon';
+
+import * as Yup from 'yup';
 
 
 const MapWithNoSSR = dynamic(() => import('../../components/Map'), {
   ssr: false,
 })
 
+type SignUpFormData = {
+  name: string;
+  about: string;
+  instructions: string;
+  password: string;
+}
+
 export default function OrphanagesMap() {
+  const formRef = useRef<FormHandles>(null);
+  const { location } = useMapContext()
+  const [orphanage, setOrphanage] = useState()
+
+  const handleSubmit = useCallback((data: SignUpFormData) => {
+    
+    
+    
+  }, [])
+
   return (
     <div className={style.pageCreateOrphanage}>
       <Sidebar />
 
       <main>
-        <form className={style.createOrphanageForm}>
+        <Form ref={formRef} onSubmit={handleSubmit} className={style.createOrphanageForm}>
           <fieldset>
             <legend>Dados</legend>
 
-            <MapWithNoSSR style={{ width: '100%', height: 280 }} />
+            <MapWithNoSSR style={{ width: '100%', height: 280 }} position={location} />
 
-            <div className={style.inputBlock}>
+            {/* <div className={style.inputBlock}>
               <label htmlFor="name">Nome</label>
               <input id="name" />
-            </div>
+            </div> */}
+            <Input name='name' label='Nome' icon={FiUser} />
 
-            <div className={style.inputBlock}>
+            {/* <div className={style.inputBlock}>
               <label htmlFor="about">Sobre <span>Máximo de 300 caracteres</span></label>
-              <textarea id="name" maxLength={300} />
-            </div>
+              <textarea id="about" maxLength={300} />
+            </div> */}
+            <Textarea name='about' label='Sobre' maxCharacter={300} />
 
             <div className={style.inputBlock}>
               <label htmlFor="images">Fotos</label>
@@ -50,28 +79,36 @@ export default function OrphanagesMap() {
           <fieldset>
             <legend>Visitação</legend>
 
-            <div className={style.inputBlock}>
+            {/* <div className={style.inputBlock}>
               <label htmlFor="instructions">Instruções</label>
               <textarea id="instructions" />
-            </div>
+            </div> */}
+            <Textarea name='instructions' label='Instruções' />
 
-            <div className={style.inputBlock}>
-              <label htmlFor="opening_hours">Nome</label>
+            {/* <div className={style.inputBlock}>
+              <label htmlFor="opening_hours">Horario para visita</label>
               <input id="opening_hours" />
-            </div>
+            </div> */}
+            <Input name='opening_hours' label='Horario para visita' />
+
+            {/* <div className={style.inputBlock}>
+              <label htmlFor="whatsapp">Whatsapp</label>
+              <input id="whatsapp" />
+            </div> */}
+            <Input name='whatsapp' label='Whatsapp' />
 
             <div className={style.inputBlock}>
               <label htmlFor="open_on_weekends">Atende fim de semana</label>
 
               <div className={style.buttonSelect}>
-                <button type="button" className="active">Sim</button>
+                <button type="button" className={style.active}>Sim</button>
                 <button type="button">Não</button>
               </div>
             </div>
           </fieldset>
 
           <PrimaryButton type="submit">Confirmar</PrimaryButton>
-        </form>
+        </Form>
       </main>
     </div>
   );
