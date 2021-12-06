65823e990b824f14a5829c633925879b52f6a677
Rodrigo de França <howdrigo.fr@gmail.com>
2021-12-06 17:48:10 -0300

    feat: implemented FormButtons component

---
+++ src/components/FormButtons/index.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import style from './styles.module.scss';
+
+type FormButtonsProps = {
+  label?: string;
+  onClick?: () => void;
+  disabled?: boolean;
+  styles?: React.CSSProperties;
+  buttonLabel: string[];
+}
+
+export default function FormButtons({ label, onClick, disabled, styles, buttonLabel }: FormButtonsProps) {
+  const [ selected, setSelected ] = useState<string>();
+
+  return (
+    <>
+      {!!label && <label htmlFor="open_on_weekends" className={style.labelButton}>{label}</label>}
+      <div className={style.inputBlock}>
+
+        <div className={style.buttonSelect} style={styles}>
+          {buttonLabel.map((text, index) => (
+            <button key={`formButton-${index}`} onClick={() => setSelected(text)} type="button" className={selected == text ? style.active : ''}>{text}</button>
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
