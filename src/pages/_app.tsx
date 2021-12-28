8f0a722f30d66a489151a9c28f1dfd8d2ab68ea5
Rodrigo de França <howdrigo.fr@gmail.com>
2021-12-28 15:56:36 -0300

    typo: rename map context provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,25 @@
 import '../styles/globals.css';
 import 'leaflet/dist/leaflet.css';
+import 'react-toastify/dist/ReactToastify.css';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { MapProvider } from '../contexts/mapContext';
+import { UserGeolocationProvider } from '../contexts/mapContext';
+import { ToastContainer } from 'react-toastify';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MapProvider>
+    <>
       <Head>
         <title>Happy :)</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="favicon.ico" />
       </Head>
-      <Component {...pageProps} />
-    </MapProvider>
+      <ToastContainer />
+      <UserGeolocationProvider>
+        <Component {...pageProps} />
+      </UserGeolocationProvider>
+    </>
   );
 }
 
