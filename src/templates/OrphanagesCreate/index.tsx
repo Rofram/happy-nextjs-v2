2c479a68c60790768c26f94059adf0d0614ae36d
Rodrigo de França <howdrigo.fr@gmail.com>
2021-12-28 15:52:53 -0300

    typo: rename map context hook

--- src/templates/OrphanagesCreate/index.tsx
+++ src/templates/OrphanagesCreate/index.tsx
@@ -1,8 +1,10 @@
-import { useState, useRef, useCallback } from 'react';
+import { useState, useRef, useCallback, ChangeEvent } from 'react';
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
 
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
+import { toast } from 'react-toastify';
 
 import PrimaryButton from "../../components/PrimaryButton";
 import Sidebar from "../../components/Sidebar";
@@ -11,14 +13,22 @@ import Textarea from '../../components/Textarea';
 
 import { FiPlus, FiUser } from "react-icons/fi";
 import mapIcon from '../../utils/map/mapIcon';
-import { useMapContext } from '../../contexts/mapContext';
+import { useUserLocationContext } from '../../contexts/mapContext';
 
 import style from './styles.module.scss';
 
 import * as Yup from 'yup';
+import api from '../../services/api';
+import getValidationErrors from '../../utils/form/getValidationErrors';
+import UploadImage from '../../components/UploadImage';
+import FormButtons from '../../components/FormButtons';
 
 
 const MapWithNoSSR = dynamic(() => import('../../components/Map'), {
+  ssr: false,
+})
+
+const LocationMarker = dynamic(() => import('../../components/LocationMarker'), {
   ssr: false,
 })
 
@@ -27,18 +37,75 @@ type SignUpFormData = {
   about: string;
   instructions: string;
   password: string;
+  opening_hours: string;
+  whatsapp: string;
+  images: File[];
 }
 
 export default function OrphanagesMap() {
   const formRef = useRef<FormHandles>(null);
-  const { location } = useMapContext()
+  const { location } = useUserLocationContext()
+  const [position, setPosition] = useState<[number, number] | null>(null);
   const [orphanage, setOrphanage] = useState()
+  const router = useRouter()
 
-  const handleSubmit = useCallback((data: SignUpFormData) => {
-    
-    
-    
-  }, [])
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const data = new FormData();
+
+    data.append('file', e.target.files![0]);
+  }
+
+  const handleSubmit = useCallback(async (data: SignUpFormData) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        about: Yup.string().min(10, 'No mínimo 10 caracteres'),
+        instructions: Yup.string().required('Instruções obrigatórias'),
+        opening_hours: Yup.string().required('Horário de funcionamento obrigatório'),
+        whatsapp: Yup.string().matches(/^\([0-9]{2}\) [0-9]{5}-[0-9]{4}$/, 'Whatsapp inválido'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      const formData = new FormData();
+
+      formData.append('name', data.name);
+      formData.append('about', data.about);
+      formData.append('instructions', data.instructions);
+      formData.append('opening_hours', data.opening_hours);
+      formData.append('whatsapp', data.whatsapp);
+
+      if (data.images) {
+        data.images.forEach(image => {
+          formData.append('images', image);
+        });
+      }
+
+      await api.post('/orphanages', data);
+
+      router.push('orphanages/success');
+
+      toast('Cadastro realizado!', {
+        type: 'success',
+      });
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+
+        return;
+      }
+
+      toast('Erro no cadastro, verifique os dados e tente novamente', {
+        type: 'error',
+      });
+    }
+  }, [router])
 
   return (
     <div className={style.pageCreateOrphanage}>
@@ -49,62 +116,27 @@ export default function OrphanagesMap() {
           <fieldset>
             <legend>Dados</legend>
 
-            <MapWithNoSSR style={{ width: '100%', height: 280 }} position={location} />
+            <MapWithNoSSR style={{ width: '100%', height: 280 }} position={location}>
+              <LocationMarker position={position} setPosition={setPosition} />
+            </MapWithNoSSR>
 
-            {/* <div className={style.inputBlock}>
-              <label htmlFor="name">Nome</label>
-              <input id="name" />
-            </div> */}
-            <Input name='name' label='Nome' icon={FiUser} />
+            <Input name='name' label='Nome' />
 
-            {/* <div className={style.inputBlock}>
-              <label htmlFor="about">Sobre <span>Máximo de 300 caracteres</span></label>
-              <textarea id="about" maxLength={300} />
-            </div> */}
             <Textarea name='about' label='Sobre' maxCharacter={300} />
 
-            <div className={style.inputBlock}>
-              <label htmlFor="images">Fotos</label>
-
-              <div className="uploaded-image">
-
-              </div>
-
-              <button className={style.newImage}>
-                <FiPlus size={24} color="#15b6d6" />
-              </button>
-            </div>
+            <UploadImage onChange={handleChange} />
           </fieldset>
 
           <fieldset>
             <legend>Visitação</legend>
 
-            {/* <div className={style.inputBlock}>
-              <label htmlFor="instructions">Instruções</label>
-              <textarea id="instructions" />
-            </div> */}
             <Textarea name='instructions' label='Instruções' />
 
-            {/* <div className={style.inputBlock}>
-              <label htmlFor="opening_hours">Horario para visita</label>
-              <input id="opening_hours" />
-            </div> */}
             <Input name='opening_hours' label='Horario para visita' />
 
-            {/* <div className={style.inputBlock}>
-              <label htmlFor="whatsapp">Whatsapp</label>
-              <input id="whatsapp" />
-            </div> */}
-            <Input name='whatsapp' label='Whatsapp' />
+            <Input name='whatsapp' label='Whatsapp' mask="(##) #####-####" />
 
-            <div className={style.inputBlock}>
-              <label htmlFor="open_on_weekends">Atende fim de semana</label>
-
-              <div className={style.buttonSelect}>
-                <button type="button" className={style.active}>Sim</button>
-                <button type="button">Não</button>
-              </div>
-            </div>
+            <FormButtons buttonLabel={['Sim', 'Não']} label='Atende fim de semana' />
           </fieldset>
 
           <PrimaryButton type="submit">Confirmar</PrimaryButton>
